9f54f99370e81ce44593d46426bce238da3286a2
AlexisPomaresDiGeronimo <95440540+AlexisPomaresDiGeronimo@users.noreply.github.com>
2026-08-04 22:09:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 const menu = document.getElementById("menu");
 const categories = document.getElementById("categories");
 
-Object.keys(menuData).forEach(category => {
-
-    const btn = document.createElement("button");
-
-    btn.textContent = category;
-
-    btn.addEventListener("click", () => {
-
-        document
-            .getElementById(category)
-            .scrollIntoView({
-                behavior: "smooth"
-            });
-
-    });
-
-    categories.appendChild(btn);
+for (const category in menuData) {
 
     const section = document.createElement("section");
 
     section.className = "category";
     section.id = category;
 
-    section.innerHTML =
-        <h2>${category}</h2>;
+    const button = document.createElement("button");
+    button.textContent = category;
+
+    button.onclick = () => {
+        section.scrollIntoView({
+            behavior: "smooth"
+        });
+    };
+
+    categories.appendChild(button);
+
+    section.innerHTML = <h2>${category}</h2>;
 
     menuData[category].forEach(item => {
 
@@ -39,17 +33,4 @@ Object.keys(menuData).forEach(category => {
     });
 
     menu.appendChild(section);
-
-});
-
-document
-.getElementById("menuBtn")
-.addEventListener("click", () => {
-
-    document
-    .getElementById("menu")
-    .scrollIntoView({
-        behavior: "smooth"
-    });
-
-});
+}
